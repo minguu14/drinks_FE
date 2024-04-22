330bbd76ac316a81e8d036d43aa812e992ab1b3f
heekwang <ekzm7040@naver.com>
2024-04-22 19:56:54 +0900

    font 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Main from "./pages/Main";
+import Main from "./Pages/Main";
 
 function App() {
   return (
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Main() {
-  return <div className="text-red-500">mainpage</div>;
+  return (
+    <div>
+      <div className="text-red-500 text-[60px] font-[400]">
+        {" "}
+        옷에서는 아무 냄새가
+      </div>
+      <div className="text-[60px] font-[700]">옷에서는 아무 냄새가</div>
+    </div>
+  );
 }
